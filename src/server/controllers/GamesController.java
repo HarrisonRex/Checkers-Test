252cc7a50df4fd8c584844e0c15fe8cd72b57742
hmacdonald75644 <40470384+hmacdonald75644@users.noreply.github.com>
2018-08-17 15:48:47 +0100

    Started on server (not working)

--- src/server/controllers/GamesController.java
+++ src/server/controllers/GamesController.java
@@ -1,9 +1,30 @@
 package server.controllers;
 
-import javax.ws.rs.Path;
+import server.Console;
+import server.models.Games;
+import server.models.services.GamesService;
+import server.models.services.UserService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.MediaType;
 
 @Path("games/")
 public class GamesController {
+    @POST
+    @Path("new")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String newGames(@CookieParam("sessionToken") Cookie sessionCookie){
+        String currentUsername = UserService.validateSessionCookie(sessionCookie);
+        if (currentUsername == null) return "Error: Invalid user session token";
 
+        Console.log("Ping from " + currentUsername);
 
+        GamesService.selectAllInto(Games.games);
+        int gameId = Games.nextId();
+
+        Games newGames = new Games(gameId, )
+
+    }
 }
